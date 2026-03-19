6883ee971e0ce0bf455991e69c06c81701a51776
jbonpimentel <jbonpimentel@users.noreply.github.com>
2026-03-19 19:18:05 -0300

    feat: montagem geral do aplicativo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="app-container">
+      <Header />
+      <Hero />
+      <Services />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
